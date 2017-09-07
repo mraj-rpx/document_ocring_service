3a7753c0fcd9d0810f5f197baa1c680d201f13c6
mraj-rpx <mraj@rpxcorp.com>
2017-09-07 16:48:51 +0530

    Removed aws keys from dotenv template.

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, "/opt/rpx/document_ocr_service"
 append :linked_files, 'config/database.yml', '.env'
 
 # default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
