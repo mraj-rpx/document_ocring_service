07601bf17d605874f2bac80aba113621a279cbce
athomas-rpx <athomas_c@rpxcorp.com>
2018-05-16 11:56:19 +0530

    re-ocr the attachments having parsing exception as no implicit conversion of nil

--- app/processors/pat_assignment_ocr_processor.rb
+++ app/processors/pat_assignment_ocr_processor.rb
@@ -11,9 +11,9 @@ class PatAssignmentOcrProcessor < OcrProcessorBase
         tempfile = s3_downloader.download
 
         ocr_content = DocsplitProcessor.new(tempfile.path, {psm: 11}).process
-        ocr_text_s3_key = "pat-assignments/#{SecureRandom.hex(3)}/#{assignment.assignment_id}.txt"
+        ocr_text_s3_key = assignment.s3_ocr_url.present? ? assignment.s3_ocr_url : "pat-assignments/#{SecureRandom.hex(3)}/#{assignment.assignment_id}.txt"
         S3Uploader.new(ocr_content[:ocr_text]).save_to_s3(ocr_text_s3_key)
-        assignment.update_attributes(s3_ocr_url: ocr_text_s3_key, ocr_exception: nil)
+        assignment.update_attributes(s3_ocr_url: ocr_text_s3_key, ocr_exception: nil, parsing_exception: nil)
       rescue => exception
         assignment.update_attributes(ocr_exception: exception)
         Honeybadger.notify(exception, context: {document_id: assignment.id, document_type: PatAssignmentAttachment})
