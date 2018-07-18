9917f61e0ed4cea00aaf640c16d8bc7e16b048a9
athomas-rpx <athomas_c@rpxcorp.com>
2018-07-18 12:34:37 +0530

    change scope to previous version and update db value ocr_exception to null

--- app/models/pat_assignment_attachment.rb
+++ app/models/pat_assignment_attachment.rb
@@ -1,6 +1,6 @@
 class PatAssignmentAttachment < ApplicationRecord
   self.table_name = 'pat_assignment_attachments'
 
-  scope :ocrable_assignments, -> { where('s3_ocr_url IS NOT NULL AND is_parsed = ? AND parsing_exception LIKE ?',false,"%no implicit conversion from nil to integer%").order(:created_at) }
+  scope :ocrable_assignments, -> { where('s3_ocr_url IS NULL AND ocr_exception IS NULL').order(:created_at) }
   scope :parsable_assignments, -> { where('s3_ocr_url IS NOT NULL AND is_parsed = ? AND parsing_exception IS NULL', false).order(:created_at) }
 end
